2241e48ba6fa75ee5da323d5833de5534a601901
Charles FEVAL <charlesfeval@microsoft.com>
2026-05-26 10:25:41 -0700

    Fix tagging script

--- .build/create-tag.sh
+++ .build/create-tag.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 VERSION=`cat VERSION`
-TAG=`v$VERSION`
+TAG="v$VERSION"
 EXISTS=`git tag | grep "$TAG"`
 
 if [ -z "$EXISTS" ]; then
   echo "Create tag $TAG"
   git tag "$TAG"
+  for REMOTE in `git remote`; do
+    git push $REMOTE --tags
+  done
 else
   echo "Tag $TAG already exists"
 fi
