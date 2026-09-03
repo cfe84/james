ffe6bea13c6272b4708071d7356a5dfe0a788948
Charles FEVAL <charlesfeval@microsoft.com>
2026-09-02 17:14:41 -0700

    Remove james public port, use internal network only

--- .build/james-deploy.sh
+++ .build/james-deploy.sh
@@ -12,7 +12,6 @@ echo "Starting ${CONTAINER_NAME} container..."
 docker create \
     --name "${CONTAINER_NAME}" \
     --restart unless-stopped \
-    -p "${QEW_PORT:-8077}:8077" \
     -e HEM_MI6_URL="${HEM_MI6_URL}" \
     ${QEW_PASSWORD:+-e QEW_PASSWORD="${QEW_PASSWORD}"} \
     -v "${JAMES_CONFIG_PATH}:/root/.config/james" \
